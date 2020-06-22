0eeca76f31c86f6c92be58cc9c62b73af2ad6a11
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-06-22 15:22:41 +0200

    cleanup

--- tests/utils.js
+++ tests/utils.js
@@ -1,7 +1,7 @@
 const { copySync, ensureDirSync, emptyDirSync } = require('fs-extra')
 const { resolve } = require('path')
 
-const buildPath = '.temp'
+const buildPath = 'temp'
 
 
 function buildATest() {

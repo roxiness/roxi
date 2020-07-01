3e9c0a29721fe8e2e0f4cc3a0230e905e421eccf
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-07-01 22:25:43 +0200

    restructure

--- __tests__/integration/dev-test.js
+++ __tests__/integration/dev-test.js
@@ -1,10 +1,11 @@
 
 const { copyATest, clearTemp } = require('../utils')
-const { execFileSync, spawnSync, spawn } = require('child_process')
+const { execFileSync, spawn } = require('child_process')
+const kill = require('tree-kill')
 require('jest-playwright-preset')
 
 
-const {chromium} = require('playwright');
+const { chromium } = require('playwright');
 let browser;
 let page;
 beforeAll(async () => {
@@ -16,17 +17,22 @@ beforeEach(async () => {
 afterEach(async () => {
   await page.close();
 });
-
+afterAll(async () => {
+  await browser.close()
+})
 
 it('can install example', async () => {
   clearTemp()
   const { path } = copyATest()
   execFileSync('pnpm install', { cwd: path, shell: true, stdio: 'inherit' })
   execFileSync('pnpm install roxi', { cwd: path, shell: true, stdio: 'inherit' })
-  spawn('pnpm', ['run', 'dev'], { cwd: path, shell: true, stdio: 'inherit' })
+  const handle = spawn('pnpm', ['run', 'dev'], { cwd: path, shell: true, stdio: 'inherit' })
+
   await require('util').promisify(setTimeout)(10000)
   await page.goto('http://dev.local:5000/')
-  const result = await page.waitForSelector('"Example app2"');
-  expect(result).toBeTruthy()
 
-}, 60000)
+  const result = await page.waitForSelector('"Example app"');
+  expect(result).toBeTruthy()
+  kill(handle.pid)
+
+}, 40000)

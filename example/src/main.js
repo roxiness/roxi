c5a6204e9f5b120dcf45432e85a24ede6178d6e3
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-07-08 19:25:36 +0200

    bundler and restructure

--- example/src/main.js
+++ example/src/main.js
@@ -1,11 +1,1 @@
 self.__ROXI
-
-
-// if ('serviceWorker' in navigator) {
-//     import('workbox-window').then(async ({ Workbox }) => {
-//         const wb = new Workbox('/sw.js')
-//         const registration = await wb.register()
-//         wb.addEventListener('installed', () => (console.log('installed service worker')))
-//         wb.addEventListener('externalinstalled', () => (console.log('installed service worker')))
-//     })
-// }

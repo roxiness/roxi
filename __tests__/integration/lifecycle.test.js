a740b0a341f73b3a8c537b258f5904a747314771
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-08-08 16:13:49 +0200

    abstract test handling

---
+++ __tests__/integration/lifecycle.test.js
@@ -0,0 +1,7 @@
+const { lifecycleTest } = require('./lifecycle')
+
+
+describe('pnpm vite', () => {
+  // lifecycleTest({ bundler: 'vite', pkgm: 'pnpm' })
+  lifecycleTest({ bundler: 'nollup', pkgm: 'pnpm', port: 5000 })
+})
--- jest.config.js
+++ jest.config.js
@@ -82,7 +82,7 @@ module.exports = {
   // moduleNameMapper: {},
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
-  modulePathIgnorePatterns: ['.history', 'utils', 'temp'],
+  modulePathIgnorePatterns: ['.history', 'utils', 'temp', 'lifecycle.js'],
 
   // Activates notifications for test results
   // notify: false,

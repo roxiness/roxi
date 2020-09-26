345ad941fecc877cc1a62a594ce786fab7d1f645
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-09-26 21:20:14 +0200

    add type definitions for plugins

--- typedef.js
+++ typedef.js
@@ -1,3 +1,18 @@
 /**
- *  @typedef {import('./lib/app').RoxiApp} RoxiApp
+ * @typedef {import('./lib/app').RoxiApp} RoxiApp
+ *
+ * @typedef {object} RoxiPlugin
+ * @prop {string} name
+ * @prop {RoxiPluginHook[]} hooks
+ *
+ * @typedef {object} RoxiPluginHook
+ * @prop {string} event
+ * @prop {RoxiPluginHookFunction|string=} condition
+ * @prop {RoxiPluginHookFunction} action
+ *
+ * @callback RoxiPluginHookFunction
+ * @param {RoxiApp} app
+ * @param {object} params
+ * @param {object} ctx
+ *
  * */

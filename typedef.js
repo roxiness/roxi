dabf05bf008503465ad90c87870f9e2bfcc7e8b0
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-09-27 18:12:25 +0200

    better types and cleanup

--- typedef.js
+++ typedef.js
@@ -1,9 +1,12 @@
 /**
- * @typedef {import('./lib/app').RoxiApp} RoxiApp
+ * @typedef {'start'|'before:config'|'config'
+ * |'after:config'|'before:bundle'|'bundle'
+ * |'after:bundle'|'router'|'end'} AppEvent
  *
  * @typedef {object} RoxiPlugin
  * @prop {string} name
  * @prop {RoxiPluginHook[]} hooks
+ * @prop {Object.<string, Function>=} dependencies
  *
  * @typedef {object} RoxiPluginHook
  * @prop {string} event
@@ -15,4 +18,12 @@
  * @param {object} params
  * @param {object} ctx
  *
- * */
+ * @typedef {object} RoxiPluginConfig
+ * @prop {string} name
+ * @prop {Object.<string, any>} params
+ *
+ * @typedef {RoxiPlugin & RoxiPluginConfig} RoxiPluginConfigured
+ *
+ * @typedef {createApp['app']} RoxiApp
+ */
+

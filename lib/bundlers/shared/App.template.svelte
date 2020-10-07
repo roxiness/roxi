61224521e503b78c365ade2c68f8444868408bab
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-10-07 18:14:31 +0200

    remove old bundlers

--- lib/bundlers/shared/App.template.svelte
+++
@@ -1,6 +0,0 @@
-import { hmr } from 'roxi/routify'
-import { Router } from 'roxi/routify'
-import { routes } from '../_roxi/.routify/routes'
-import App from './App.svelte'
-
-const app = hmr(Router, { target: document.body, props: { routes } }, 'routify-app')
